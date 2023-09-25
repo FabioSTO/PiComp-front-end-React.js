1d786f8d2312af7d226eb45359124b9c2ea32862
FabioSTO <f.torrado@udc.es>
2023-09-25 19:58:31 +0200

    Modified to upload to S3

--- src/hooks/submitImage.js
+++ src/hooks/submitImage.js
@@ -4,12 +4,26 @@ const endpointUrl = apiUrl + '/users/submitImage'; // http://localhost:3001/user
 
 const submitImage = async (imageName, imageComp, username) => {
   try {
+    // Crear una promesa para leer la imagen en Base64
+    const readImage = () => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => {
+          resolve(reader.result);
+        };
+        reader.onerror = reject;
+        reader.readAsDataURL(imageComp);
+      });
+    };
+
+    const imageBase64 = await readImage(); // Lee imagen en Base64
+
     const response = await fetch(endpointUrl, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({imageName, imageComp, username})
+      body: JSON.stringify({imageName, imageBase64, username})
     });
 
     if (response.ok) {
